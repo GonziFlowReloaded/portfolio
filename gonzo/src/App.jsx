a2c9c4ee20ed6eba41017762c82904117ca5b09d
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-01-03 22:10:36 -0300

    first commit

---
+++ gonzo/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section id="home"></section>
+      <section id="about"></section>
+      <section id="projects"></section>
+      <section id="contact"></section>
+    </div>
+  );
+};
+
+export default App;
